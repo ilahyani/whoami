12f572e3be6849eb9fcef3623ec5ebfdb03d2dd6
ilahyani <lahyani799@gmail.com>
2023-06-19 17:57:21 +0100

    Chat UI components hierarchy updated

--- frontend/src/Components/ChatPage.jsx
+++ frontend/src/Components/ChatPage.jsx
@@ -1,12 +1,16 @@
 import ChatSidebar from "./chatComponents/ChatSidebar";
 import ChatBody from "./chatComponents/ChatBody";
+import MessageForm from "./chatComponents/MessageForm";
 import "../myStyle.css";
 
 export default function ChatPage() {
   return (
     <div className="chat_container">
       <ChatSidebar />
-      <ChatBody />
+      <div className="chat_body">
+        <ChatBody />
+        <MessageForm />
+      </div>
     </div>
   );
 }
--- frontend/src/Components/chatComponents/ChatBody.jsx
+++ frontend/src/Components/chatComponents/ChatBody.jsx
@@ -1,15 +1,11 @@
-import MessageForm from "./MessageForm";
 import ChatBodyHeader from "./ChatBodyHeader";
 import ChatBodyMessages from "./ChatbodyMessages";
 
 export default function ChatBody() {
   return (
-    <div className="chat_body">
-      <div className="header_and_chat">
-        <ChatBodyHeader />
-        <ChatBodyMessages />
-      </div>
-      <MessageForm />
+    <div className="header_and_chat">
+      <ChatBodyHeader />
+      <ChatBodyMessages />
     </div>
   );
 }
