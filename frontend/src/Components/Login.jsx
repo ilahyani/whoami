e94d28abf535a9f1babbb47816cad58433756dc4
ilahyani <lahyani799@gmail.com>
2023-06-21 04:52:44 +0100

    generating temporary unique usernames

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,9 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import "../myStyle.css";
 
+function generateRandomUsername() {
+  const randomString = Math.random().toString(10).substring(5);
+  const username = `User${randomString}`;
+  return username;
+}
+
 export default function Login() {
   const navigate = useNavigate();
   const handleClick = () => {
+    localStorage.setItem("userName", generateRandomUsername());
+    console.log("UserName", localStorage.getItem("userName"));
     navigate("/chat");
   };
 
@@ -12,7 +20,7 @@ export default function Login() {
       <h2 className="login_header"> Welcome to WHOAMI </h2>
       <h4 className="login_subheader"> Whoever you are </h4>
       <button onClick={handleClick} className="login_CTA">
-        Sign in with intra
+        Get in
       </button>
     </div>
   );
