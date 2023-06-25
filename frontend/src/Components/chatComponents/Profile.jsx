9dc601a3900a1c6e783e1a1d7d628d4b0691c19e
ilahyani <lahyani799@gmail.com>
2023-06-25 18:00:53 +0100

    using find method instead of mapping the users array

--- frontend/src/Components/chatComponents/Profile.jsx
+++ frontend/src/Components/chatComponents/Profile.jsx
@@ -1,17 +1,13 @@
 export default function Profile({ users, socket }) {
+  const user = users?.find((user) => user.socketID === socket.id);
   return (
     <div className="contact_list" key={socket.id}>
-      {users.map((user) => {
-        if (socket.id === user.socketID)
-          return (
-            <div className="contact_box" key={user.socketID}>
-              <div className="avatar">
-                <img src={user.avatar} alt="avatar" />
-              </div>
-              <div className="username">{user.username}</div>
-            </div>
-          );
-      })}
+      <div className="contact_box" key={user?.socketID}>
+        <div className="avatar">
+          <img src={user?.avatar} alt="avatar" />
+        </div>
+        <div className="username">{user?.username}</div>
+      </div>
     </div>
   );
 }
