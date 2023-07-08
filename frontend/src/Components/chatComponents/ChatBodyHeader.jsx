2ba3b8b8f374766003ff7896ee6ddaa3b17d4d38
ilahyani <lahyani799@gmail.com>
2023-07-08 13:35:47 +0100

    Chat Body Header update

--- frontend/src/Components/chatComponents/ChatBodyHeader.jsx
+++ frontend/src/Components/chatComponents/ChatBodyHeader.jsx
@@ -1,26 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus, faUserMinus } from "@fortawesome/free-solid-svg-icons";
-
 export default function ChatBodyHeader() {
   return (
     <div className="chat_body_header">
-      <h2>Chat</h2>
-      <div className="icons">
-        <a href="#">
-          <FontAwesomeIcon
-            icon={faUserPlus}
-            size="xl"
-            style={{ color: "#6f6fe0" }}
-          />
-        </a>
-        <a href="#">
-          <FontAwesomeIcon
-            icon={faUserMinus}
-            size="xl"
-            style={{ color: "#6f6fe0" }}
-          />
-        </a>
-      </div>
+      <h2>Global Chat</h2>
     </div>
   );
 }
