0ba1f473c5887ca35a58d8ba43d2ef61ebe7183b
ilahyani <lahyani799@gmail.com>
2024-04-24 23:40:49 +0100

    removing strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,4 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./Components/App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
