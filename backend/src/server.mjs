3f4c68eb116e72ff924600fb6cf9d00344d4f5b6
ilahyani <lahyani799@gmail.com>
2024-04-28 14:10:20 +0100

    saving messages in a redis DB

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -3,14 +3,16 @@ import { createServer } from "http";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
 import cors from "cors";
+import Redis from "ioredis";
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
 const httpServer = createServer(app);
+const redis = new Redis(process.env.REDIS_URL);
 
-app.use(cors);
+app.use(cors());
 
 let users = [];
 const io = new Server(httpServer, { cors: process.env.CLIENT_DOMAIN });
@@ -20,7 +22,11 @@ io.on("connection", (socket) => {
     io.emit("newUserResponse", users);
     socket.disconnect();
   });
-  socket.on("message", (data) => {
+  socket.on("message", async (data) => {
+    const key = `message:${new Date().getTime()}`;
+    await redis.rpush("msg_keys", key);
+    await redis.hmset(key, data);
+    console.log(`msg ${key} saved to DB`);
     io.emit("messageResponse", data);
   });
   socket.on("newUser", (data) => {
@@ -30,6 +36,16 @@ io.on("connection", (socket) => {
   socket.on("getUsers", () => {
     io.emit("newUserResponse", [...users]);
   });
+});
+
+app.get("/messages/get", async (request, response) => {
+  const msgKeys = await redis.lrange("msg_keys", 0, -1);
+  const msgs = [];
+  for (let key of msgKeys) {
+    const msg = await redis.hgetall(key);
+    msgs.push(msg);
+  }
+  return response.status(200).json(msgs);
 });
 
 httpServer.listen(port, () => {
